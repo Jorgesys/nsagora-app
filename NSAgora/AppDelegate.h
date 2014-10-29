87812acf02fc198fccabff4d26a035c50377de07
Alex Cristea <alexcristea@me.com>
2014-10-29 18:42:09 +0200

    Initial Commit

---
+++ NSAgora/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSAgora
+//
+//  Created by Alex Cristea on 29/10/14.
+//  Copyright (c) 2014 NSAgora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSAgora/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSAgora
+//
+//  Created by Alex Cristea on 29/10/14.
+//  Copyright (c) 2014 NSAgora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
